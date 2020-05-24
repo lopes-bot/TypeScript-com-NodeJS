3d85df94be82374af4e721bb8ec9554f2758f27f
unknown <andrelopessfla@gmail.com>
2020-05-23 23:52:51 -0300

    criado metodo de controller para cadastro de usuario

--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,7 @@ import UserController from './controllers/Usercontroller'
 const routes = Router()
 
 routes.get('/users', UserController.index)
+routes.post('/users', UserController.store)
+
 
 export default routes
