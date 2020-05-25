22aac6e59aec2871b14d21f5814b7eebb67cdf33
unknown <andrelopessfla@gmail.com>
2020-05-25 00:27:17 -0300

    criado rota e controller para deletar usuario

--- src/controllers/Usercontroller.ts
+++ src/controllers/Usercontroller.ts
@@ -14,6 +14,23 @@ class UserController {
 
     return res.json(user)
   }
+
+  public async Delete (req:Request, res:Response): Promise<Response> {
+    const id = req.params
+    const user = await User.deleteOne({ id: id }, (err) => {
+      if (err) {
+        return res.status(400).json({
+          erro: true,
+          messege: 'Erro ao deletar Usuario'
+        })
+      }
+    })
+    return res.json({
+      erro: false,
+      messege: 'Usuario deletado com sucesso!',
+      user
+    })
+  }
 }
 
 export default new UserController()
--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ const routes = Router()
 
 routes.get('/users', UserController.index)
 routes.post('/users', UserController.store)
+routes.delete('/users/delete/:id', UserController.Delete)
 
 
 export default routes
